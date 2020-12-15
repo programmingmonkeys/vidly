470df213bb181e367e2a1c22119956868665aaed
codingChewie <codingChewie@gmail.com>
2020-12-15 15:45:24 -0600

    destructuring arguments

--- vidly/src/components/Navbar.jsx
+++ vidly/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Navbar = (props) => {
+const Navbar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#!">
-          Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+          Navbar <span className="badge badge-pill badge-secondary">{totalCounters}</span>
         </a>
       </div>
     </nav>
