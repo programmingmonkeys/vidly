8c96c9d1e79552eb0f0cc20740feb096723e9b7a
codingChewie <codingChewie@gmail.com>
2020-12-14 18:06:50 -0600

    specifying children

---
+++ counter-app/src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>hello</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    )
+  }
+}
+export default Counter
