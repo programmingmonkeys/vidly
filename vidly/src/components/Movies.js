73b018e0a08938543f51925f552f566c573b8a68
codingChewie <codingChewie@gmail.com>
2020-12-15 11:39:42 -0600

    deleting a movie

--- vidly/src/components/Movies.js
+++ vidly/src/components/Movies.js
@@ -6,7 +6,11 @@ class Movies extends Component {
     movies: getMovies(),
   }
 
-  handleDelete = (movie) => {}
+  handleDelete = (movie) => {
+    // console.log(movie)
+    const movies = this.state.movies.filter((m) => m._id !== movie._id)
+    this.setState({ movies })
+  }
 
   render() {
     return (
@@ -17,15 +21,21 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th />
           </tr>
         </thead>
         <tbody>
-          {this.state.movies.map((movie, i) => (
-            <tr key={i}>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
