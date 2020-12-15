0abb6b30db74dc4263b866333137d725d971bec4
codingChewie <codingChewie@gmail.com>
2020-12-15 14:10:44 -0600

    removing the local state

--- vidly/src/components/Counters.jsx
+++ vidly/src/components/Counters.jsx
@@ -11,13 +11,22 @@ class Counters extends Component {
     ],
   }
 
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters]
+    const index = counters.indexOf(counter)
+
+    counters[index] = { ...counter }
+    counters[index].value++
+    this.setState({ counters })
+  }
+
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId)
     this.setState({ counters })
   }
 
   handleReset = () => {
-    this.state.counters.map((c) => {
+    const counters = this.state.counters.map((c) => {
       c.value = 0
       return c
     })
@@ -27,11 +36,11 @@ class Counters extends Component {
   render() {
     return (
       <div>
-        <button onClick={handleReset} className="btn btn-primary btn-sm m-2">
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />
+          <Counter key={counter.id} onDelete={this.handleDelete} onIncrement={this.handleIncrement} counter={counter} />
         ))}
       </div>
     )
