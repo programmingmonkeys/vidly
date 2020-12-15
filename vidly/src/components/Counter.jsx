2b315e1fdf71f789637a3f2d210a0ef9823a3637
codingChewie <codingChewie@gmail.com>
2020-12-15 12:33:58 -0600

    passing data to components

--- vidly/src/components/Counter.jsx
+++ vidly/src/components/Counter.jsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   }
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 })
+    this.setState({ count: this.state.value + 1 })
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-'
+    classes += this.state.value === 0 ? 'warning' : 'primary'
+    return classes
+  }
+
+  formatCount() {
+    const { value: count } = this.state
+    return count === 0 ? 'Zero' : count
   }
 
   render() {
@@ -18,17 +29,6 @@ class Counter extends Component {
         </button>
       </div>
     )
-  }
-
-  getBadgeClasses() {
-    let classes = 'badge m-2 badge-'
-    classes += this.state.count === 0 ? 'warning' : 'primary'
-    return classes
-  }
-
-  formatCount() {
-    const { count } = this.state
-    return count === 0 ? 'Zero' : count
   }
 }
 
