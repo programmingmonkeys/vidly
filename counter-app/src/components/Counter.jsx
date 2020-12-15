deac86ced5ff5331dc7531d569fe217b0c3ffb13
codingChewie <codingChewie@gmail.com>
2020-12-15 10:42:54 -0600

    handling events

--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -3,26 +3,39 @@ import React, { Component } from 'react'
 class Counter extends Component {
   state = {
     count: 0,
-    tags: [],
   }
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>No tags</p>
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>No tags</p>
 
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    )
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   )
+  // }
+
+  handleIncrement() {
+    console.log('increment clicked', this.state.count)
+  }
+
+  formatCount() {}
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-'
+    classes += this.state.count === 0 ? 'warning' : 'primary'
+    return classes
   }
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && 'Please create a new tag'}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">
+          Increment
+        </button>
       </div>
     )
   }
