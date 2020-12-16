014d9dfccdaa2b9d8599526ea12be8d0ab61ac95
codingChewie <codingChewie@gmail.com>
2020-12-16 15:49:41 -0600

    adding the NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 
+import NavBar from './components/NavBar'
 import Movies from './components/Movies'
 import Customers from './components/Customers'
 import Rentals from './components/Rentals'
@@ -11,16 +12,19 @@ import './App.css'
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="not-found" />
-        </Switch>
-      </div>
+      <>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="not-found" />
+          </Switch>
+        </div>
+      </>
     )
   }
 }
