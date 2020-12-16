461010299efbead81416c882b3499ed7b84dca3b
codingChewie <codingChewie@gmail.com>
2020-12-15 22:02:37 -0600

    pagination paginating data

--- vidly/src/components/Movies.jsx
+++ vidly/src/components/Movies.jsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import Like from './common/Like'
 import Pagination from './common/Pagination'
 import { getMovies } from '../services/fakeMovieService'
+import { paginate } from '../utils/paginate'
 
 class Movies extends Component {
   state = {
@@ -31,9 +32,11 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies
-    const { pageSize, currentPage } = this.state
+    const { pageSize, currentPage, movies: allMovies } = this.state
 
     if (count === 0) return <p>There are no movies in the database</p>
+
+    const movies = paginate(allMovies, currentPage, pageSize)
 
     return (
       <Fragment>
@@ -50,7 +53,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map((movie) => (
+            {movies.map((movie) => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
