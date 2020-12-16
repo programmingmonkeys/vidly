71ad4b482b73d8be3bd5a78a948ebec46695d4bb
codingChewie <codingChewie@gmail.com>
2020-12-15 22:42:31 -0600

    filtering adding all genres

--- vidly/src/components/Movies.jsx
+++ vidly/src/components/Movies.jsx
@@ -15,7 +15,8 @@ class Movies extends Component {
   }
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() })
+    const genres = [{ name: 'All Genres' }, ...getGenres()]
+    this.setState({ movies: getMovies(), genres })
   }
 
   handleDelete = (movie) => {
@@ -38,7 +39,7 @@ class Movies extends Component {
   }
 
   handleGenreSelect = (genre) => {
-    this.setState({ selectedGenre: genre })
+    this.setState({ selectedGenre: genre, currentPage: 1 })
   }
 
   render() {
@@ -47,7 +48,8 @@ class Movies extends Component {
 
     if (count === 0) return <p>There are no movies in the database</p>
 
-    const filtered = selectedGenre ? allMovies.filter((m) => m.genre._id === selectedGenre._id) : allMovies
+    const filtered =
+      selectedGenre && selectedGenre._id ? allMovies.filter((m) => m.genre._id === selectedGenre._id) : allMovies
     const movies = paginate(filtered, currentPage, pageSize)
 
     return (
