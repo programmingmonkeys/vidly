9e96e5823c154e2a0ee561f1cb8c1f9f114abd49
codingChewie <codingChewie@gmail.com>
2020-12-16 08:23:10 -0600

    sorting implementing sorting

--- vidly/src/components/Movies.jsx
+++ vidly/src/components/Movies.jsx
@@ -5,6 +5,7 @@ import { getMovies } from '../services/fakeMovieService'
 import { paginate } from '../utils/paginate'
 import { getGenres } from '../services/fakeGenreService'
 import ListGroup from './common/ListGroup'
+import _ from 'lodash'
 
 class Movies extends Component {
   state = {
@@ -12,6 +13,7 @@ class Movies extends Component {
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    sortColumn: { path: 'title', order: 'asc' },
   }
 
   componentDidMount() {
@@ -43,18 +45,30 @@ class Movies extends Component {
   }
 
   handleSort = (path) => {
-    console.log(path)
+    const sortColumn = { ...this.state.sortColumn }
+
+    if (sortColumn.path === path) {
+      sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc'
+    } else {
+      sortColumn.path = path
+      sortColumn.order = 'asc'
+    }
+
+    this.setState({ sortColumn })
   }
 
   render() {
     const { length: count } = this.state.movies
-    const { pageSize, currentPage, selectedGenre, movies: allMovies } = this.state
+    const { pageSize, currentPage, sortColumn, selectedGenre, movies: allMovies } = this.state
 
     if (count === 0) return <p>There are no movies in the database</p>
 
     const filtered =
       selectedGenre && selectedGenre._id ? allMovies.filter((m) => m.genre._id === selectedGenre._id) : allMovies
-    const movies = paginate(filtered, currentPage, pageSize)
+
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
+
+    const movies = paginate(sorted, currentPage, pageSize)
 
     return (
       <div className="row">
