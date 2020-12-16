0433a3665bef00f64744ee9684924148b2c3ca5c
codingChewie <codingChewie@gmail.com>
2020-12-16 13:12:12 -0600

    destructuring arguments

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
 
-const Pagination = (props) => {
-  const { itemsCount, pageSize, currentPage, onPageChange } = props
-  console.log(currentPage)
+const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
+  // console.log(currentPage)
 
   const pagesCount = Math.ceil(itemsCount / pageSize)
   if (pagesCount === 1) return null
@@ -14,7 +13,7 @@ const Pagination = (props) => {
   return (
     <nav>
       <ul className="pagination">
-        {pages.map((page) => (
+        {pages.map(page => (
           <li key={page} className={page === currentPage ? 'page-item active' : 'page-item'}>
             <a className="page-link" onClick={() => onPageChange(page)} href="#!">
               {page}
