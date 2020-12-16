b09e41f2c4698c8e380a96d1e9e3c65e88ed8474
codingChewie <codingChewie@gmail.com>
2020-12-16 08:29:34 -0600

    sorting moving responsibility

--- vidly/src/components/Movies.jsx
+++ vidly/src/components/Movies.jsx
@@ -44,16 +44,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 })
   }
 
-  handleSort = (path) => {
-    const sortColumn = { ...this.state.sortColumn }
-
-    if (sortColumn.path === path) {
-      sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc'
-    } else {
-      sortColumn.path = path
-      sortColumn.order = 'asc'
-    }
-
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn })
   }
 
@@ -81,7 +72,13 @@ class Movies extends Component {
         </div>
         <div className="col">
           <p>Showing {filtered.length} movies in the database.</p>
-          <MoviesTable movies={movies} onLike={this.handleLike} onDelete={this.handleDelete} onSort={this.handleSort} />
+          <MoviesTable
+            movies={movies}
+            sortColumn={sortColumn}
+            onLike={this.handleLike}
+            onDelete={this.handleDelete}
+            onSort={this.handleSort}
+          />
           <Pagination
             itemsCount={filtered.length}
             pageSize={pageSize}
