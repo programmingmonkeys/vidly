f42a84b912b3bbe9be264c081241413c4015cd63
codingChewie <codingChewie@gmail.com>
2020-12-16 15:27:38 -0600

    adding react router

---
+++ src/components/MovieForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MovieForm = () => {
+  return <h1></h1>
+}
+
+export default MovieForm
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
 
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
+import { reduceRight } from 'lodash'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 )
