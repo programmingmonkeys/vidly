c82a37bef22f2a9a3227150e8d0217cc9e64f724
codingChewie <codingChewie@gmail.com>
2020-12-16 08:10:08 -0600

    sorting extracting MoviesTable

--- vidly/src/components/Movies.jsx
+++ vidly/src/components/Movies.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Like from './common/Like'
+import MoviesTable from './MoviesTable'
 import Pagination from './common/Pagination'
 import { getMovies } from '../services/fakeMovieService'
 import { paginate } from '../utils/paginate'
@@ -63,36 +63,7 @@ class Movies extends Component {
         </div>
         <div className="col">
           <p>Showing {filtered.length} movies in the database.</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th />
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like liked={movie.liked} onLikeToggle={() => this.handleLike(movie)} />
-                  </td>
-                  <td>
-                    <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <MoviesTable movies={movies} onLike={this.handleLike} onDelete={this.handleDelete} />
           <Pagination
             itemsCount={filtered.length}
             pageSize={pageSize}
