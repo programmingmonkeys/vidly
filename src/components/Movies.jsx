0c092658baf16e4dbae1398d098e94226de20f4a
codingChewie <codingChewie@gmail.com>
2020-12-17 11:17:33 -0600

    search movies

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -5,6 +5,7 @@ import { getMovies } from '../services/fakeMovieService'
 import { paginate } from '../utils/paginate'
 import { getGenres } from '../services/fakeGenreService'
 import ListGroup from './common/ListGroup'
+import SearchBox from './SearchBox'
 import _ from 'lodash'
 
 import { Link } from 'react-router-dom'
@@ -15,6 +16,8 @@ class Movies extends Component {
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    searchQuery: '',
+    selectedGenre: null,
     sortColumn: { path: 'title', order: 'asc' },
   }
 
@@ -46,17 +49,29 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 })
   }
 
+  handleSearch = query => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 })
+  }
+
   handleSort = sortColumn => {
     this.setState({ sortColumn })
   }
 
-  getPageData = () => {
-    const { pageSize, currentPage, sortColumn, selectedGenre, movies: allMovies } = this.state
+  getPagedData = () => {
+    const {
+      pageSize,
+      currentPage,
+      sortColumn,
+      selectedGenre,
+      searchQuery,
+      movies: allMovies,
+    } = this.state
 
-    const filtered =
-      selectedGenre && selectedGenre._id
-        ? allMovies.filter(m => m.genre._id === selectedGenre._id)
-        : allMovies
+    let filtered = allMovies
+    if (searchQuery)
+      filtered = allMovies.filter(m => m.title.toLowerCase().startsWith(searchQuery.toLowerCase()))
+    else if (selectedGenre && selectedGenre._id)
+      filtered = allMovies.filter(m => m.genre._id === selectedGenre._id)
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
 
@@ -87,6 +102,7 @@ class Movies extends Component {
             New Movie
           </Link>
           <p>Showing {totalCount} movies in the database.</p>
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
