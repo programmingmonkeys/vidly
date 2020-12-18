f6ab87f6db525b704c1403158deb545bd8c045b6
codingChewie <codingChewie@gmail.com>
2020-12-18 10:40:01 -0600

    populating the form

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Joi from 'joi-browser'
 import Form from './common/Form'
-import { getMovie, saveMovie } from '../services/fakeMovieService'
-import { getGenres } from '../services/fakeGenreService'
+import { getMovie, saveMovie } from '../services/MovieService'
+import { getGenres } from '../services/GenreService'
 
 class MovieForm extends Form {
   state = {
@@ -24,17 +24,19 @@ class MovieForm extends Form {
     dailyRentalRate: Joi.number().required().min(0).max(10).label('Daily Rental Rate'),
   }
 
-  componentDidMount() {
-    const genres = getGenres()
+  async componentDidMount() {
+    const { data: genres } = await getGenres()
     this.setState({ genres })
 
     const movieId = this.props.match.params.id
     if (movieId === 'new') return
 
-    const movie = getMovie(movieId)
-    if (!movie) return this.props.history.replace('/not-found')
-
-    this.setState({ data: this.mapToViewModel(movie) })
+    try {
+      const { data: movie } = await getMovie(movieId)
+      this.setState({ data: this.mapToViewModel(movie) })
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404) return this.props.history.replace('/not-found')
+    }
   }
 
   mapToViewModel(movie) {
--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -5,6 +5,11 @@ const apiEndPoint = `${apiUrl}/movies`
 export function getMovies() {
   return http.get(apiEndPoint)
 }
+export function getMovie(movieId) {
+  return http.get(`${apiEndPoint}/${movieId}`)
+}
+
+export function saveMovie(movie) {}
 
 export function deleteMovie(movieId) {
   return http.delete(`${apiEndPoint}/${movieId}`)
