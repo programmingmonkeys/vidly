7f75457ce8e3eb401c698fc8a9117fc623431ed4
codingChewie <codingChewie@gmail.com>
2020-12-18 11:24:32 -0600

    saving the movie

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -9,7 +9,15 @@ export function getMovie(movieId) {
   return http.get(`${apiEndPoint}/${movieId}`)
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie }
+    delete body._id
+    return http.put(`${apiEndPoint}/${movie._id}`, body)
+  }
+
+  return http.post(apiEndPoint, movie)
+}
 
 export function deleteMovie(movieId) {
   return http.delete(`${apiEndPoint}/${movieId}`)
