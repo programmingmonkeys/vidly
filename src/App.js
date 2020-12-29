3926d2837242f5b802d48de93a6a64973f825b55
codingChewie <codingChewie@gmail.com>
2020-12-29 14:24:54 -0600

    displaying the current user on navbar

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
       this.setState({ user })
     } catch (error) {}
   }
+
   render() {
     return (
       <>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
-const NavBar = () => {
+const NavBar = ({user}) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
@@ -20,29 +20,27 @@ const NavBar = () => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/movies">
+            <NavLink className="nav-item nav-link" to="/movies">
               Movies
             </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/customers">
+            <NavLink className="nav-item nav-link" to="/customers">
               Customers
             </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/rentals">
+            <NavLink className="nav-item nav-link" to="/rentals">
               Rentals
             </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/login">
+            {!user && <><NavLink className="nav-item nav-link" to="/login">
               Login
             </NavLink>
-          </li>
           <NavLink className="nav-item nav-link" to="/register">
-            Register
-          </NavLink>
+              Register
+          </NavLink></>}
+          {user && <><NavLink className="nav-item nav-link" to="/profile">
+              {user.name}
+            </NavLink>
+          <NavLink className="nav-item nav-link" to="/logout">
+            Logout
+          </NavLink></>}
         </ul>
       </div>
     </nav>
